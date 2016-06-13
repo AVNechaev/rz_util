f4499c5b9bd23bc615a29b7dc1656564cb222e2f
ANechaev <ANechaev@102new>
2016-06-13 20:49:55 +0300

    ...

--- include/rz_util.hrl
+++ include/rz_util.hrl
@@ -3,4 +3,15 @@
 
 -type(instr_name() :: binary()).
 
+-record(candle, {
+  name :: instr_name(),
+  open = 0 :: float(),
+  close = 0 :: float(),
+  high = 0 :: float(),
+  low = 0 :: float(),
+  vol = 0 :: float(),
+  bid = 0 :: float(), % bid & ask используются только в текущей свече
+  ask = 0 :: float()
+}).
+
 -endif.
